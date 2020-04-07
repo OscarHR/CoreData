2468621c782ccbb17be1dd3a335860d623d531a9
Oscar Hernandez <oscar.hernandez@midtronics.com>
2020-04-07 13:32:55 -0600

    Initial commit

---
+++ GameDatabaseManager/GameDatabaseManager.h
@@ -0,0 +1,19 @@
+//
+//  GameDatabaseManager.h
+//  GameDatabaseManager
+//
+//  Created by Oscar Hernandez on 07/04/20.
+//  Copyright © 2020 Oscar Hernandez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GameDatabaseManager.
+FOUNDATION_EXPORT double GameDatabaseManagerVersionNumber;
+
+//! Project version string for GameDatabaseManager.
+FOUNDATION_EXPORT const unsigned char GameDatabaseManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GameDatabaseManager/PublicHeader.h>
+
+
